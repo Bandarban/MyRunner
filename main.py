f354ba64c7c70f9646163ff16be001d7de08c501
Bandarban <Bandarban@users.noreply.github.com>
2018-01-21 15:24:17 +0500

    12

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Menu(GameState):
 
         Menu.exit_btn_act = ImgObj('img/Exit_selected.png', 0, 0)
         Menu.exit_btn_act.move(200, 350)
-        print("git test")
+        print("git test123")
 
     @staticmethod
     def update():
