e01d3043660d0cc6b862674e21245ff07739a216
Bandarban <Bandarban@users.noreply.github.com>
2018-01-26 21:17:17 +0500

    Menu is fine

--- methods.py
+++ methods.py
@@ -8,7 +8,7 @@ def load_image(img_path, colorkey=None, scale=None):
         image = pygame.image.load(img_path)
         if scale is not None:
             image = pygame.transform.smoothscale(image, (scale[0], scale[1]))
-        image = image.convert()
+            image = image.convert()
         if colorkey is not None:
             if colorkey is -1:
                 colorkey = image.get_at((0, 0))
